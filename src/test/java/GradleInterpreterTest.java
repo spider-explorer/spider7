2413b96cd18263d4d2fb0bdd527d92301b1185e6
JavaCommons Technologies <javacommons@gmail.com>
2023-07-20 04:16:07 +0900

    .

--- src/test/java/GradleInterpreterTest.java
+++ src/test/java/GradleInterpreterTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.util.Eval;
 
@@ -14,10 +15,13 @@ class GradleInterpreterTest {
 		System.out.println(Eval.me("33*3"));
 		System.out.println(Eval.me("'foo'.toUpperCase()"));
 		
-		var shell = new GroovyShell(GradleInterpreterTest.class.getClassLoader());
+		var sharedData = new Binding();
+		sharedData.setProperty("vm", new global.VM8());
+		var shell = new GroovyShell(GradleInterpreterTest.class.getClassLoader(), sharedData);
 		shell.evaluate("""
-				def vm = new global.VM8()
-				vm.print(123.45D)
+				def vm2 = new global.VM8()
+				vm2.print(123.45D)
+				vm.print(777D)
 				""");
 	}
 
